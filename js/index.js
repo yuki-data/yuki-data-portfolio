e11b5db6d8fa47fb9764fb24a5d62b127fd8b965
yuki-data <45279969+yuki-data@users.noreply.github.com>
2019-05-07 00:03:13 +0900

    init: portfolio site

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  "use strict";
+  $('nav a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    var id = this.hash;
+    var $target = $(id);
+    $('html, body').animate({
+      'scrollTop': $target.offset().top
+    }, 300);
+  });
+});
