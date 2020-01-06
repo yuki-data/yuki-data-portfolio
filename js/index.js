65cbb0b45f38108334771716ef04b27d65d00c5b
yuki-data <45279969+yuki-data@users.noreply.github.com>
2020-01-06 13:44:23 +0900

    navbarのリンクで該当するセクションがなければスクロールしない

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
   "use strict";
-  $('nav a[href^="#"]').on('click', function (e) {
+  $('nav a[href^="#"]').on("click", function(e) {
     e.preventDefault();
     var id = this.hash;
+    if (!id) {
+      return;
+    }
     var $target = $(id);
-    $('html, body').animate({
-      'scrollTop': $target.offset().top
-    }, 300);
+    $("html, body").animate(
+      {
+        scrollTop: $target.offset().top
+      },
+      300
+    );
   });
 });
